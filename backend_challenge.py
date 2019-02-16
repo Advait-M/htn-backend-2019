4468ebcb655865db87d6dd3f14e693c1014086c9
Advait-M <advait.maybhate9@gmail.com>
2019-02-15 20:39:34 -0500

    Clean up of code (removing debugging statements, fixing indentation and spacing)

--- backend_challenge.py
+++ backend_challenge.py
@@ -1,17 +1,17 @@
 import sqlite3
-import urllib.request, json
+import urllib.request
+import json
+
 with urllib.request.urlopen("https://htn-interviews.firebaseio.com/users.json") as url:
     data = json.loads(url.read().decode())
-    print(data)
-    print(type(data))
 
-DB_NAME = "example8.db"
+DB_NAME = "htn.db"
 conn = sqlite3.connect(DB_NAME)
-#
+
 c = conn.cursor()
-#
+
 c.execute('''
-CREATE TABLE IF NOT EXISTS users 
+CREATE TABLE IF NOT EXISTS users
 (email text primary key, name text, picture text, company text, phone text, latitude real, longitude real)
 ''')
 c.execute('''
@@ -21,14 +21,22 @@ CREATE TABLE IF NOT EXISTS skills
 
 for i in data:
     try:
-        c.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?)", (i["email"], i["name"], i["picture"], i["company"], i["phone"], i["latitude"], i["longitude"]))
+        c.execute(
+            "INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?)",
+            (i["email"],
+             i["name"],
+                i["picture"],
+                i["company"],
+                i["phone"],
+                i["latitude"],
+                i["longitude"]))
     except sqlite3.IntegrityError:
         print("User already in database:", i["email"])
         continue
     for skill in i["skills"]:
-        c.execute("INSERT INTO skills VALUES (?, ?, ?)", (i["email"], skill["name"], skill["rating"]))
+        c.execute("INSERT INTO skills VALUES (?, ?, ?)",
+                  (i["email"], skill["name"], skill["rating"]))
 
 conn.commit()
 
 conn.close()
-
--- flask_queries.py
+++ flask_queries.py
@@ -1,79 +1,33 @@
 import sqlite3
-import json
-from flask import Flask
-from flask import jsonify
-from flask import request
-from flask import abort
+from flask import Flask, jsonify, request, abort
 
-#TODO: Add user functionality - done
-#TODO: Delete user functionality - done
-# TODO: Advanced skills querying
-
-
-DB_NAME = "example8.db"
+DB_NAME = "htn.db"
 SQLITE_TYPES = {"null": None, "integer": int, "real": float, "text": str}
-
-# conn = sqlite3.connect(DB_NAME)
-
-# c = conn.cursor()
-# c.row_factory = sqlite3.Row
-
-# dict(zip([1,2,3,4], [a,b,c,d]))
-
-# print(c.execute("SELECT * FROM users"))
-# for i in c.execute("SELECT * FROM users"):
-#     print(i.keys())
-#     print(tuple(i))
-#     print(dict(zip(i.keys(), tuple(i))))
-
-
-
-# cursor = conn.cursor()
-# cursor.row_factory = sqlite3.Row
-# result = []
-# for row in c.execute("SELECT * FROM users"):
-#     user = dict(zip(row.keys(), tuple(row)))
-#     skills = []
-#     print(user["email"])
-#     # print(tuple(user["email"]))
-#     for row_skill in cursor.execute("SELECT name, rating FROM skills WHERE email = ?", [user["email"]]):
-#         skills.append(tuple(row_skill))
-#     user["skills"] = skills
-#     result.append(user)
-#
-# print("RES", result)
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return "Welcome to Advait's Hack the North Backend Challenge 2019. Feel free to explore the various endpoints!", 200
 
 
-## REFACTOR TO USE FOREIGN KEY
 @app.route('/users', methods=['GET'])
 def get_users():
     with sqlite3.connect(DB_NAME) as conn:
         c = conn.cursor()
         c.row_factory = sqlite3.Row
         result = []
-        # print(c.execute("SELECT * FROM users"))
-        # print([i for i in c.execute("SELECT * FROM users")])
-        # all_users = c.execute("SELECT * FROM users").fetchall()
-        # print([i for i in all_users])
         for row in c.execute("SELECT * FROM users").fetchall():
             user = dict(zip(row.keys(), tuple(row)))
-            skills = []
-            # print(user["email"])
-            # print(tuple(user["email"]))
-            skills = [dict(zip(row_skill.keys(), tuple(row_skill))) for row_skill in c.execute("SELECT name, rating FROM skills WHERE email = ?", [user["email"]]).fetchall()]
 
-            # for row_skill in c.execute("SELECT name, rating FROM skills WHERE email = ?", [user["email"]]):
-            #     skills.append(tuple(row_skill))
-            user["skills"] = skills
+            user["skills"] = [dict(zip(row_skill.keys(), tuple(row_skill))) for row_skill in c.execute(
+                "SELECT name, rating FROM skills WHERE email = ?", [user["email"]]).fetchall()]
+
             result.append(user)
+
         return jsonify(result)
+
 
 @app.route('/users/<user_email>', methods=['GET'])
 def get_user(user_email):
@@ -81,52 +35,42 @@ def get_user(user_email):
         c = conn.cursor()
         c.row_factory = sqlite3.Row
 
-        query_result = c.execute("SELECT * FROM users WHERE email = ?", [user_email])
-        # print(query_result.fetchall())
+        query_result = c.execute(
+            "SELECT * FROM users WHERE email = ?",
+            [user_email])
+
         results = query_result.fetchall()
-        print("rc", query_result.rowcount)
+
         if len(results) == 0:
             return jsonify([])
         else:
             row = results[0]
             user = dict(zip(row.keys(), tuple(row)))
-            user["skills"] = [dict(zip(row_skill.keys(), tuple(row_skill))) for row_skill in c.execute("SELECT name, rating FROM skills WHERE email = ?", [user["email"]]).fetchall()]
+            user["skills"] = [dict(zip(row_skill.keys(), tuple(row_skill))) for row_skill in c.execute(
+                "SELECT name, rating FROM skills WHERE email = ?", [user["email"]]).fetchall()]
+
             return jsonify(user)
+
 
 def check_if_exists(user_email):
     with sqlite3.connect(DB_NAME) as conn:
         c = conn.cursor()
         c.row_factory = sqlite3.Row
 
-        query_result = c.execute("SELECT * FROM users WHERE email = ?", [user_email])
-        # print(query_result.fetchall())
+        query_result = c.execute(
+            "SELECT * FROM users WHERE email = ?",
+            [user_email])
+
         results = query_result.fetchall()
-        print("rc", query_result.rowcount)
+
         if len(results) == 0:
             return False
         else:
             return True
 
-# def delete_skill_if_exists(user_email, skill_name):
-#     with sqlite3.connect(DB_NAME) as conn:
-#         c = conn.cursor()
-#         c.row_factory = sqlite3.Row
-#
-#         query_result = c.execute("DELETE * FROM skills WHERE email = ?", [user_email])
-#         # print(query_result.fetchall())
-#         results = query_result.fetchall()
-#         print("rc", query_result.rowcount)
-#         if len(results) == 0:
-#             return False
-#         else:
-#             return True
 
-# TODO: add skills
 @app.route('/users/<user_email>', methods=['PUT'])
 def update_user(user_email):
-    print(user_email)
-    print(request.get_json())
-    print(request.get_data())
     if not check_if_exists(user_email):
         abort(400, "User with email address provided does not exist.")
     given_to_update = request.get_json()
@@ -135,44 +79,42 @@ def update_user(user_email):
         c.row_factory = sqlite3.Row
         keys = {}
         for i in c.execute("PRAGMA table_info(users)").fetchall():
-            print("i222", tuple(i))
             # Get column names not including primary key (email)
             if i[5] == 0:
                 keys[i[1]] = SQLITE_TYPES[i[2]]
-        print(keys)
-        print([(j[0], type(j[1])) for j in given_to_update.items()])
         to_update = {}
         for j in given_to_update.items():
-            print("j", j)
             if j[0] in keys:
-                if type(j[1]) != keys[j[0]]:
-                    print(j, "errror")
-                    print(j[1], keys[j[0]])
-                    abort(400, "Type of field '" + j[0] + "' does not match required type of " + keys[j[0]].__name__)
-                    # return "ERROR"
+                if not isinstance(j[1], keys[j[0]]):
+                    abort(400, "Type of field '" +
+                          j[0] +
+                          "' does not match required type of " +
+                          keys[j[0]].__name__)
                 else:
                     to_update[j[0]] = j[1]
-
-        # to_update["skills"] = given_to_update["skills"]
-        # to_update = dict([j for j in given_to_update.items() if (j[0], type(j[1])) in keys])
-        print(to_update)
-        print("here")
-        print(",".join([j[0] + " = ? " for j in to_update.items()]))
-        print([j[1] for j in to_update.items()])
 
         if "skills" in given_to_update:
             validate_skills(given_to_update)
             for i in given_to_update["skills"]:
-                c.execute("DELETE FROM skills WHERE email = ? AND name = ?", [user_email, i["name"]])
-                c.execute("INSERT INTO skills VALUES (?, ?, ?)", [user_email, i["name"], i["rating"]])
+                c.execute(
+                    "DELETE FROM skills WHERE email = ? AND name = ?", [
+                        user_email, i["name"]])
+                c.execute(
+                    "INSERT INTO skills VALUES (?, ?, ?)", [
+                        user_email, i["name"], i["rating"]])
 
         if len(to_update) > 0:
-            update_columns = ",".join([j[0] + " = ? " for j in to_update.items()])
+            update_columns = ",".join(
+                [j[0] + " = ? " for j in to_update.items()])
             update_values = [j[1] for j in to_update.items()]
             update_values.append(user_email)
-            c.execute("UPDATE users SET " + update_columns + " WHERE email = ?", update_values).fetchall()
+            c.execute(
+                "UPDATE users SET " +
+                update_columns +
+                " WHERE email = ?",
+                update_values).fetchall()
         conn.commit()
-    print(get_user(user_email) == [])
+
     return get_user(user_email)
 
 
@@ -182,20 +124,21 @@ def validate_skills(data):
         c.row_factory = sqlite3.Row
         skill_keys = {}
         for i in c.execute("PRAGMA table_info(skills)").fetchall():
-            print("i222", tuple(i))
             # Get column names not including foreign key (email)
             if i[1] != "email":
                 skill_keys[i[1]] = SQLITE_TYPES[i[2]]
-        print("sk", skill_keys)
+
         for i in data["skills"]:
-            if type(i) != dict:
-                abort(400, "Skill information must be provided nested dictionaries.")
+            if not isinstance(i, dict):
+                abort(
+                    400, "Skill information must be provided nested dictionaries.")
 
             for j in skill_keys:
                 if j not in i:
                     abort(400, "Skill missing required field: " + j)
-                if type(j) != type(i[j]):
-                    abort(400, "Skill field of wrong type (must be string): " + j)
+                if not isinstance(j, type(i[j])):
+                    abort(
+                        400, "Skill field of wrong type (must be string): " + j)
 
 
 # TODO: type checking - done
@@ -207,43 +150,35 @@ def add_user():
         c.row_factory = sqlite3.Row
         keys = {}
         for i in c.execute("PRAGMA table_info(users)").fetchall():
-            print("i222", tuple(i))
             # Get column names including primary key
             keys[i[1]] = SQLITE_TYPES[i[2]]
-        print(keys)
+
         for i in keys:
             if i not in data:
                 abort(400, "Missing field: " + i)
-            if keys[i] != type(data[i]):
+            if not isinstance(data[i], keys[i]):
                 abort(400, "Incorrect type: " + i)
-        if type(data["skills"]) != list:
+        if not isinstance(data["skills"], list):
             abort(400, "Skills must be a list.")
 
         validate_skills(data)
-        # skill_keys = {}
-        # for i in c.execute("PRAGMA table_info(skills)").fetchall():
-        #     print("i222", tuple(i))
-        #     # Get column names not including foreign key (email)
-        #     if i[1] != "email":
-        #         skill_keys[i[1]] = SQLITE_TYPES[i[2]]
-        # print("sk", skill_keys)
-        # for i in data["skills"]:
-        #     if type(i) != dict:
-        #         abort(400, "Skill information must be provided nested dictionaries.")
-        #
-        #     for j in skill_keys:
-        #         if j not in i:
-        #             abort(400, "Skill missing required field: " + j)
-        #         if type(j) != type(i[j]):
-        #             abort(400, "Skill field of wrong type (must be string): " + j)
-
         try:
-            c.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?)", (data["email"], data["name"], data["picture"], data["company"], data["phone"], data["latitude"], data["longitude"]))
+            c.execute(
+                "INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?)",
+                (data["email"],
+                 data["name"],
+                    data["picture"],
+                    data["company"],
+                    data["phone"],
+                    data["latitude"],
+                    data["longitude"]))
         except sqlite3.IntegrityError:
             abort(400, "User already in database: " + data["email"])
         for skill in data["skills"]:
-            c.execute("INSERT INTO skills VALUES (?, ?, ?)", (data["email"], skill["name"], skill["rating"]))
+            c.execute("INSERT INTO skills VALUES (?, ?, ?)",
+                      (data["email"], skill["name"], skill["rating"]))
     return get_user(data["email"])
+
 
 @app.route('/users/<user_email>', methods=['DELETE'])
 def delete_user(user_email):
@@ -260,7 +195,6 @@ def delete_user(user_email):
 @app.route('/skills', methods=['GET'])
 def get_skills():
     args = request.args
-    print(args)
     with sqlite3.connect(DB_NAME) as conn:
         c = conn.cursor()
         c.row_factory = sqlite3.Row
@@ -284,13 +218,12 @@ def get_skills():
             result.append({"name": row[0], "frequency": row[1]})
         return jsonify(result)
 
+
 @app.route('/skills/<user_email>', methods=['GET'])
 def get_skills_user(user_email):
     if not check_if_exists(user_email):
         abort(400, "User with email address provided does not exist.")
     args = request.args
-    print(args)
-    print(args["min_rating"])
     with sqlite3.connect(DB_NAME) as conn:
         c = conn.cursor()
         c.row_factory = sqlite3.Row
@@ -308,12 +241,9 @@ def get_skills_user(user_email):
             except ValueError:
                 abort(400, "max_rating must be an integer")
             additional += " AND rating <= ?"
-        # final_result = []
-        # for i in c.execute("SELECT name, rating FROM skills WHERE email = ?" + additional, values).fetchall()
-        #     final_result.append()
-        return jsonify([dict(zip(row_skill.keys(), tuple(row_skill))) for row_skill in c.execute("SELECT name, rating FROM skills WHERE email = ?" + additional, values).fetchall()])
 
-
+        return jsonify([dict(zip(row_skill.keys(), tuple(row_skill))) for row_skill in c.execute(
+            "SELECT name, rating FROM skills WHERE email = ?" + additional, values).fetchall()])
 
 
 @app.route('/skills/frequency/<skill_name>', methods=['GET'])
@@ -321,25 +251,29 @@ def get_skill_frequency(skill_name):
     with sqlite3.connect(DB_NAME) as conn:
         c = conn.cursor()
         c.row_factory = sqlite3.Row
-        query_result = c.execute("SELECT count(*) FROM skills WHERE name = ?", [skill_name])
-        print(query_result)
-        # print(query_result.fetchone()[0])
+        query_result = c.execute(
+            "SELECT count(*) FROM skills WHERE name = ?",
+            [skill_name])
+
         return jsonify({"skill_frequency": query_result.fetchone()[0]})
+
 
 @app.route('/skills/average/<skill_name>', methods=['GET'])
 def get_skill_average(skill_name):
     with sqlite3.connect(DB_NAME) as conn:
         c = conn.cursor()
         c.row_factory = sqlite3.Row
-        query_result = c.execute("SELECT AVG(rating) FROM skills WHERE name = ?", [skill_name])
-        print(query_result)
-        # print(query_result.fetchone()[0])
+        query_result = c.execute(
+            "SELECT AVG(rating) FROM skills WHERE name = ?",
+            [skill_name])
+
         frequency = query_result.fetchone()[0]
-        print(frequency)
+
         if frequency is None:
             abort(400, "No skills found with specified name")
+
         return jsonify({"skill_frequency": frequency})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
